ff1643092c3ed6644b5c90cd7c004d10c642d254
Andrei Dubinko <andreidubinko@Andreis-MacBook-Pro.local>
2021-10-04 13:22:23 +0300

    terms / games modal

--- js/cookies.js
+++ js/cookies.js
@@ -1,10 +1,15 @@
 let overlay = document.querySelector('.overlay');
 let popup = document.querySelector('.cookies');
 let accept_btn = document.querySelector('.accept');
+let cookies_btn = document.querySelector('.cookies-btn');
 // let reject_btn = document.querySelector('.reject');
 
-
 accept_btn.addEventListener('click', function(){
+    overlay.style.display = "none";
+    popup.style.display = "none";
+});
+
+cookies_btn.addEventListener('click', function(){
     overlay.style.display = "none";
     popup.style.display = "none";
 });
